64ad33de8b6f9b5e5fc2990146e4dea99fd4a030
Stanislavmg <stanislav.as1998@gmail.com>
2024-01-27 18:56:21 +0300

    ft_putstr_fd

--- libft.h
+++ libft.h
@@ -32,4 +32,5 @@ char	**ft_split(char const *s, char c);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_strteri(char *s, void (*f)(unsigned int, char*));
 void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
 #endif
