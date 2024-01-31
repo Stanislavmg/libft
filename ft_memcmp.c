fce8c49c2f890c4164e5de13b9dfa0728ef81e38
Stanislavmg <stanislav.as1998@gmail.com>
2024-01-31 19:27:32 +0300

    ft_memcmp_upd

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -2,11 +2,16 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	while (n && (*(unsigned char *)s1) == *(unsigned char *)s2)
-		n--;
-	if (*(unsigned char *)s1 > *(unsigned char *)s2)
-		return (1);
-	if (*(unsigned char *)s1 < *(unsigned char *)s2)
-		return (-1);
-	return (0);
+	size_t	i;
+
+	i = 0;
+	if (!n)
+		return (0);
+	while (((unsigned char *)s1)[i] == ((unsigned char *)s2)[i] && *(unsigned char *)s1 && *(unsigned char *)s2)
+	{
+		if (i + 1 == n)
+			return (0);
+		i++;
+	}
+	return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
 }
