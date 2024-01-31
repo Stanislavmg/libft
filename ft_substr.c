dcbba481130effd0e51f665f235cf80959cbc7bc
Stanislavmg <stanislav.as1998@gmail.com>
2024-01-31 20:17:16 +0300

    ft_substr_upd

--- ft_substr.c
+++ ft_substr.c
@@ -5,13 +5,12 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*d;
 	size_t	i;
 
-	d = NULL;
 	i = 0;
-	if (!s || (start + len > ft_strlen(s)))
-		return (d);
+	if (!s)
+		return (NULL);
 	d = malloc(len + 1);
 	if (!d)
-		return (d);
+		return (NULL);
 	while (i < len)
 		d[i++] = s[start++];
 	d[i] = 0;
